9d47d6c6121d6aef11f1d6763fcf601fe4d1f2cc
Mani Damirchilou <manidamirchilou@gmail.com>
2024-10-19 21:40:13 +0330

    Create "checkbox" component

---
+++ resources/views/components/checkbox.blade.php
@@ -0,0 +1,6 @@
+<div class="form-control">
+    <label class="label cursor-pointer">
+        <span class="label-text">{{$slot}}</span>
+        <input type="checkbox" class="checkbox" {{$attributes}} />
+    </label>
+</div>
